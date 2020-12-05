b90f2986584583add5bec553a273e6cdda61112a
jasonbrackman <brackman@gmail.com>
2020-12-05 09:28:12 -0800

    day_05 - Refactor; added a Window class and moved repeated code into the class for calc. upper / lower bounds

--- day_05.py
+++ day_05.py
@@ -1,38 +1,51 @@
 import helpers
+from typing import List
 
 
-def get_seat_id(sample):
-    upper_col = 127
-    lower_col = 0
+class Window:
+    def __init__(self, lower, upper):
+        self.lower: int = lower
+        self.upper: int = upper
 
-    upper_row = 7
-    lower_row = 0
+    def keep_lower_half(self):
+        self.upper = (self.upper - self.lower) // 2 + self.lower
 
-    for x in sample:
-        if x == "F":  # F - lower half
-            upper_col = (upper_col - lower_col) // 2 + lower_col
-        elif x == "B":  # B - upper half
-            lower_col = (lower_col + upper_col) // 2 + 1
-        elif x == "L":  # L - lower half
-            upper_row = (upper_row - lower_row) // 2 + lower_row
-        elif x == "R":  # R = upper half
-            lower_row = (lower_row + upper_row) // 2 + 1
-
-    return lower_col * 8 + lower_row
+    def keep_upper_half(self):
+        self.lower = (self.upper + self.lower) // 2 + 1
 
 
-def get_first_missing_seat(items):
-    for i, x in enumerate(items, items[0]):
+def get_seat_from_id(seat_id: str) -> int:
+    magic_number = 8  # puzzle provided
+    col: Window = Window(0, 127)
+    row: Window = Window(0, 7)
+
+    for _id in seat_id:
+        if _id == "F":
+            col.keep_lower_half()
+        elif _id == "B":
+            col.keep_upper_half()
+        elif _id == "L":
+            row.keep_lower_half()
+        elif _id == "R":
+            row.keep_upper_half()
+
+    assert col.lower == col.upper and row.lower == row.upper
+    return col.lower * magic_number + row.lower
+
+
+def get_first_missing_seat_id(seat_ids: List) -> int:
+    for i, x in enumerate(seat_ids, seat_ids[0]):
         if i != x:
             return i
 
 
 if __name__ == "__main__":
     lines = helpers.get_lines(r'./data/day_05.txt')
-    items = sorted(get_seat_id(line) for line in lines)
 
-    part1 = items[-1]
-    part2 = get_first_missing_seat(items)
+    seat_numbers = sorted(get_seat_from_id(line) for line in lines)
 
-    assert part1 == 930
-    assert part2 == 515
+    part1 = seat_numbers[-1]
+    part2 = get_first_missing_seat_id(seat_numbers)
+
+    assert part1 == 930, f"Expected 930; received {part1}"
+    assert part2 == 515, f"Expected 515; received {part2}"
