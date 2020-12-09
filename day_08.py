272a124fd7884504d81de3ff59e6a102f2e0f693
jasonbrackman <brackman@gmail.com>
2020-12-08 22:05:36 -0800

    day_09

--- day_08.py
+++ day_08.py
@@ -1,5 +1,65 @@
 import helpers
 
+
+class HackException(Exception):
+    pass
+
+
+class Machine(object):
+
+    def __init__(self, path):
+        self.__accumulator = 0
+        self.idx = 0
+        self.program = self.load(path)
+        self.seen = {0: 1}
+
+    @property
+    def result(self):
+        return self.__accumulator
+
+    def run(self, preparse=None, debug=False):
+        """Program will exit with an index error when complete."""
+
+        self.__accumulator = 0
+        self.idx = 0
+        self.seen = {0:1}
+
+        while True:
+            if preparse:
+                preparse()
+
+            arg, num = self.program[self.idx]
+
+            if arg == "acc":
+                self.__accumulator += num
+                self.idx += 1
+            elif arg == "jmp":
+                self.idx += num
+            elif arg == "nop":
+                self.idx += 1
+
+            if debug:
+                self.debug(arg, num)
+
+    @staticmethod
+    def load(path):
+        compiled = []
+
+        lines = helpers.get_lines(path)
+        for line in lines:
+            arg, num = line.split()
+            num = int(num)
+            compiled.append((arg, num))
+
+        return compiled
+
+    def debug(self, arg, num):
+        if self.idx not in self.seen:
+            self.seen[self.idx] = 0
+        self.seen[self.idx] += 1
+        # print(f"{arg:<3} {num:<5} | ({self.seen[self.idx]:>3}) {self.idx:<5} {self.__accumulator:<5}")
+
+
 def part01(lines):
 
     accumulator = 0
@@ -51,7 +111,38 @@ def part02(lines):
                 pointer += 1
 
 
+def hack01():
+    if m.seen[m.idx] > 1:
+        raise HackException(m.result)
+
+
 if __name__ == "__main__":
     lines = helpers.get_lines(r'./data/day_08.txt')
+
+    m = Machine(r'./data/day_08.txt')
+
+    # Part01
+    try:
+        m.run(preparse=hack01, debug=True)
+    except HackException as e:
+        print(e.args[0])
+
+    # part 02
+    idxs = [i for i, l in enumerate(m.program) if l[0] == "jmp"]
+    for idx in idxs:
+        print(idx)
+        old = m.program[idx]
+        try:
+            m.program[idx] = ("nop", 0)
+            m.run(preparse=hack01, debug=True)
+        except IndexError:
+            print("Part2:", m.result)
+        except HackException as e:
+            # print(e.args[0])
+            # print(m.result)
+            pass
+        m.program[idx] = old
+
+
     assert part01(lines) == 1584
     assert part02(lines) == 920
---
+++ day_09.py
@@ -0,0 +1,56 @@
+import helpers
+
+INPUT_PATH = r'./data/day_09.txt'
+PREAMBLE = 25
+
+
+def parse_input():
+    lines = helpers.get_lines(INPUT_PATH)
+    return [int(i.strip()) for i in lines]
+
+
+def is_pair(block, next):
+    for b in block:
+        test = next - b if next > b else b - next
+        if test in block and b != test:
+            return True
+    return False
+
+
+def part01(nums):
+    current = 0
+    while True:
+        index = current + PREAMBLE + 1
+        block = sorted(nums[current:index])
+        next = nums[index]
+        if not is_pair(block, next):
+            return next
+        current += 1
+
+
+def part_02(needle):
+    low = 0
+    high = 1
+    while True:
+        r = nums[low:high]
+        test = sum(r)
+        if test == needle:
+            return sum((min(r), max(r)))
+        else:
+            if test > needle:
+                low += 1
+            else:
+                high += 1
+
+
+if __name__ == "__main__":
+    nums = parse_input()
+    p1 = part01(nums)
+    p2 = part_02(p1)
+
+    assert p1 == 22406676
+    assert p2 == 2942387
+
+
+
+
